7a01088e33b3983c0c6e7715bfc291b1e30ddfe6
navono <navono007@gmail.com>
2021-08-17 08:30:35 +0800

    fix: 修改 eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'airbnb-typescript',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+};
