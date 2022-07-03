389dbaf6afdb47068de53c2c30e09e959617989f
pqx <pingqixing@supcon.com>
2022-07-03 15:06:06 +0800

    feat: 新增 ffi-napi 加载 dll 的示例代码

---
+++ dll/src/dll_for_node.cpp
@@ -0,0 +1,22 @@
+﻿// dll_for_node.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "dll_for_node.h"
+
+
+// 这是导出变量的一个示例
+//DLLFORNODE_API int ndllfornode=0;
+
+// 这是导出函数的一个示例。
+DLLFORNODE_API int add(int a, int b)
+{
+    return a + b;
+}
+
+//// 这是已导出类的构造函数。
+//Cdllfornode::Cdllfornode()
+//{
+//    return;
+//}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import { greeter } from './hello';
+import * as ffi from 'ffi-napi';
+
+const dllPath = `${__dirname}/../dll/dllfornode`;
+const dllForNode = ffi.Library(dllPath, {
+  'add': ['int', ['int', 'int']]
+});
 
 (async () => {
   const rsp = await greeter('z');
   console.log(rsp);
+
+  // current.atoi('1234'); // 1234
 })();
+
+console.log(`${__dirname}`);
+console.log('dllForNode', dllForNode);
+console.log('add', dllForNode.add(1, 2));
