1a46c57b998c2cbf9b9939ec340fae7091d4678b
navono <navono007@gmail.com>
2022-08-26 15:18:07 +0800

    fix: 在 mqtt 与 http 间同步

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, UseInterceptors, Logger } from '@nestjs/common';
 import * as ID from 'nodejs-unique-numeric-id-generator';
+import * as MQTT from 'async-mqtt';
+import { Semaphore } from 'async-mutex';
 
 import { ResponseInterceptor } from '@common/interceptors/response.interceptor';
 import { getUTCTimestamp } from '@util/util';
@@ -7,12 +9,64 @@ import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  constructor(private readonly logger: Logger) { }
+  // private mutex = new Mutex();
+  private semaphore = new Semaphore(1);
+
+  private mqttClient: MQTT.AsyncMqttClient
+
+  constructor(private readonly logger: Logger) {
+    this.mqttClient = MQTT.connect('mqtt://localhost:1883');
+
+    this.mqttClient.on('connect', (packet: any) => {
+      console.error('packet', packet)
+      this.mqttClient.subscribe('supcon').then(val => {
+        this.logger.debug('recv mqtt ', val)
+      }).catch(reasone => {
+        this.logger.error('mqtt error', reasone)
+      });
+    });
+  }
 
   @Get('/')
   @UseInterceptors(ResponseInterceptor)
-  hello() {
-    this.logger.debug('Root')
+  async hello() {
+    const [semNumber, isRelease] = await this.semaphore.acquire();
+    this.logger.debug('semNumber', semNumber);
+
+    this.mqttClient.on('message', (topic, message) => {
+
+      // message is Buffer
+      console.log(topic, message.toString());
+      this.semaphore.runExclusive(() => { Promise.resolve('10') });
+
+      isRelease();
+    });
+
+    await this.mqttClient.publish('supcon.monitor.writetag', 'hello from nestjs');
+
+    await this.semaphore.waitForUnlock();
+    // this.logger.debug('semaphore unlock');
+
+    // let taskCalls = 0;
+
+    // const awaitUnlockWrapper = async () => {
+    //   await this.semaphore.waitForUnlock();
+    //   taskCalls++;
+    // };
+
+    // const lock = this.semaphore.acquire();
+    // this.semaphore.acquire();
+
+    // awaitUnlockWrapper();
+    // awaitUnlockWrapper();
+    // await clock.tickAsync(0);
+
+    // // assert.strictEqual(taskCalls, 0);
+
+    // const [, releaser] = await lock;
+    // releaser();
+    // await clock.tickAsync(0);
+
     return 'Hello Nestjs Template!'
   }
 
